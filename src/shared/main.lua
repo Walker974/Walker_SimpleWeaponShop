559811ff15400ef02b7c195691d5a83a7b023c09
Walker <80550093+Walker974@users.noreply.github.com>
2022-11-19 00:10:02 +0100

    Initial commit

---
+++ src/shared/main.lua
@@ -0,0 +1,7 @@
+--[[
+  This file is part of Walker974 Scripts.
+  Copyright (c) Walker974  - All Rights Reserved.
+  Unauthorized using, copying, modifying and/or distributing of this file,
+  via any medium is strictly prohibited. This code is confidential.
+--]]
+---@author Walker974
